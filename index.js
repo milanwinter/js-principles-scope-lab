e4f1fbcf1a725fdee9e6e8a50f953c28614307a2
milanwinter <milanwinter8@gmail.com>
2020-11-17 16:28:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = "BOB"
+}
+
+function setBestCustomer() {
+    return bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoritecustomer = "bob"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoritecustomer = "milie"
+}
